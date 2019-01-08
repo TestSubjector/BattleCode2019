f0f53ea03431c76c84893b5882f9673edc85c707
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2019-01-08 20:10:06 +0530

    Out of bound movement eradicated

---
+++ bc19-scaffold/bots/InitialBot1/pilgrims.py
@@ -0,0 +1,90 @@
+def pilgrim(robot):
+    carry_karb = robot.me.karbonite 
+    carry_fuel = robot.me.fuel
+    pos_x = robot.me.x
+    pos_y = robot.me.y
+    
+    if carry_fuel > 69 or carry_karb > 17:
+        robot.log("Robot wants to unload")
+    
+    # robot.log('Position is ' + str(pos_x) + ' ' + str(pos_y))
+    ab =  pilgrim_mine(robot)
+    if ab !=0:
+        return ab
+    else:
+        bc = pilgrim_move(robot)
+        if bc !=0:
+            return bc
+
+def pilgrim_move(robot):
+    pos_x = robot.me.x
+    pos_y = robot.me.y
+    passable_map = robot.get_passable_map()
+    karb_map = robot.get_karbonite_map()
+    fuel_map = robot.get_fuel_map()
+    bounds_map = len(robot.map)
+
+    # May change for impossible resources
+    # SE
+    if (pos_x + 1 < bounds_map and pos_y + 1 <bounds_map) and (karb_map[pos_x + 1][pos_y + 1] == 1 or fuel_map[pos_x + 1][pos_y + 1] == 1):
+        return robot.move(1, 1)
+    # E
+    elif (pos_x + 1 < bounds_map) and (karb_map[pos_x + 1][pos_y + 0] == 1 or fuel_map[pos_x + 1][pos_y + 0] == 1):
+        return robot.move(0, 1)
+    # NE
+    elif (pos_x + 1 < bounds_map and pos_y - 1 >= 0) and (karb_map[pos_x + 1][pos_y - 1] == 1 or fuel_map[pos_x + 1][pos_y - 1] == 1):
+        return robot.move(-1, 1)
+    # N
+    elif (pos_y - 1 >= 0) and (karb_map[pos_x + 0][pos_y - 1] == 1 or fuel_map[pos_x + 0][pos_y - 1] == 1):
+        return robot.move(-1, 0)
+    # NW
+    elif (pos_x - 1 >= 0 and pos_y - 1 >= 0) and (karb_map[pos_x - 1][pos_y - 1] == 1 or fuel_map[pos_x - 1][pos_y - 1] == 1):
+        return robot.move(-1, -1)
+    # W
+    elif (pos_x - 1 >= 0) and (karb_map[pos_x - 1][pos_y + 0] == 1 or fuel_map[pos_x - 1][pos_y + 0] == 1):
+        return robot.move(0, -1)
+    # SW
+    elif (pos_x - 1 >= 0 and pos_y + 1 < bounds_map) and (karb_map[pos_x - 1][pos_y + 1] == 1 or fuel_map[pos_x - 1][pos_y + 1] == 1):
+        return robot.move(1, -1)
+    # S
+    elif (pos_x >= 0 and pos_y + 1 < bounds_map) and (karb_map[pos_x + 0][pos_y + 1] == 1 or fuel_map[pos_x + 0][pos_y + 1] == 1):
+        return robot.move(1, 0)
+    else:
+        # Just move
+        if pos_x + 1 < bounds_map and pos_y + 1 <bounds_map and passable_map[pos_x + 1][pos_y + 1] == 1:
+            return robot.move(1, 1)
+        # E
+        elif pos_x + 1 < bounds_map and passable_map[pos_x + 1][pos_y + 0] == 1:
+            return robot.move(0, 1)
+        # SE
+        elif pos_x + 1 < bounds_map and pos_y - 1 >= 0 and passable_map[pos_x + 1][pos_y - 1] == 1:
+            return robot.move(-1, 1)
+        # S
+        elif pos_y - 1 >= 0 and passable_map[pos_x + 0][pos_y - 1] == 1:
+            return robot.move(-1, 1)
+        # SW
+        elif pos_x - 1 >= 0 and pos_y - 1 >= 0 and passable_map[pos_x - 1][pos_y - 1] == 1:
+            return robot.move(-1, -1)
+        # W
+        elif pos_x - 1 >= 0 and passable_map[pos_x - 1][pos_y + 0] == 1:
+            return robot.move(0, -1)
+        # NW
+        elif pos_x - 1 >= 0 and passable_map[pos_x - 1][pos_y + 1] == 1:
+            return robot.move(-1, 1)
+        # N
+        elif pos_x >= 0 and pos_y + 1 < bounds_map and passable_map[pos_x + 0][pos_y + 1] == 1:
+            return robot.move(1, 0)
+        else: 
+            return 0
+
+def pilgrim_mine(robot):
+    pos_x = robot.me.x
+    pos_y = robot.me.y
+
+    karb_map = robot.get_karbonite_map()
+    fuel_map = robot.get_fuel_map()
+    if karb_map[pos_x][pos_y] == 1 or fuel_map[pos_x][pos_y] == 1:
+        return robot.mine()
+    else:
+        return 0        
+   
--- bc19-scaffold/bots/InitialBot1/robot.py
+++ bc19-scaffold/bots/InitialBot1/robot.py
@@ -2,6 +2,8 @@ from battlecode import BCAbstractRobot, SPECS
 import battlecode as bc
 import random
 
+from crusaders import *
+from pilgrims import *
 
 __pragma__('iconv')
 __pragma__('tconv')
@@ -11,6 +13,7 @@ __pragma__('tconv')
 
 # Helper Function
 
+
 def go_home(self):
     unit_map = self.getVisibleRobotMap()
     unit_type  = SPECS['CASTLE']
@@ -18,108 +21,6 @@ def go_home(self):
 
 def find_unit_type(self, map):
     None
-
-# Pilgrims
-
-def pilgrim_mine(robot):
-    pos_x = robot.me.x
-    pos_y = robot.me.y
-
-    karb_map = robot.get_karbonite_map()
-    fuel_map = robot.get_fuel_map()
-    if karb_map[pos_x][pos_y] == 1 or fuel_map[pos_x][pos_y] == 1:
-        return robot.mine()
-    else:
-        return 0        
-        
-def pilgrim_move(robot):
-    pos_x = robot.me.x
-    pos_y = robot.me.y
-    passable_map = robot.get_passable_map()
-    karb_map = robot.get_karbonite_map()
-    fuel_map = robot.get_fuel_map()
-    # May change for impossible resources
-    # NE
-    if karb_map[pos_x + 1][pos_y + 1] == 1 or fuel_map[pos_x + 1][pos_y + 1] == 1:
-        return robot.move(1, 1)
-    # E
-    elif karb_map[pos_x + 1][pos_y + 0] == 1 or fuel_map[pos_x + 1][pos_y + 0] == 1:
-        return robot.move(0, 1)
-    # SE
-    elif karb_map[pos_x + 1][pos_y - 1] == 1 or fuel_map[pos_x + 1][pos_y - 1] == 1:
-        return robot.move(-1, 1)
-    # S
-    elif karb_map[pos_x + 0][pos_y - 1] == 1 or fuel_map[pos_x + 0][pos_y - 1] == 1:
-        return robot.move(-1, 0)
-    # SW
-    elif karb_map[pos_x - 1][pos_y - 1] == 1 or fuel_map[pos_x - 1][pos_y - 1] == 1:
-        return robot.move(-1, -1)
-    # W
-    elif karb_map[pos_x - 1][pos_y + 0] == 1 or fuel_map[pos_x - 1][pos_y + 0] == 1:
-        return robot.move(0, -1)
-    # NW
-    elif karb_map[pos_x - 1][pos_y + 1] == 1 or fuel_map[pos_x - 1][pos_y + 1] == 1:
-        return robot.move(1, -1)
-    # N
-    elif karb_map[pos_x + 0][pos_y + 1] == 1 or fuel_map[pos_x + 0][pos_y + 1] == 1:
-        return robot.move(1, 0)
-    else:
-        # Just move
-        if passable_map[pos_x + 1][pos_y + 1] == 1:
-            return robot.move(1, 1)
-        # E
-        elif passable_map[pos_x + 1][pos_y + 0] == 1:
-            return robot.move(0, 1)
-        # SE
-        elif passable_map[pos_x + 1][pos_y - 1] == 1:
-            return robot.move(-1, 1)
-        # S
-        elif passable_map[pos_x + 0][pos_y - 1] == 1:
-            return robot.move(-1, 1)
-        # SW
-        elif passable_map[pos_x - 1][pos_y - 1] == 1:
-            return robot.move(-1, -1)
-        # W
-        elif passable_map[pos_x - 1][pos_y + 0] == 1:
-            return robot.move(0, -1)
-        # NW
-        elif passable_map[pos_x - 1][pos_y + 1] == 1:
-            return robot.move(-1, 1)
-        # N
-        elif passable_map[pos_x + 0][pos_y + 1] == 1:
-            return robot.move(1, 0)
-        else: 
-            return 0
-
-def pilgrim(robot):
-    carry_karb = robot.me.karbonite 
-    carry_fuel = robot.me.fuel
-    pos_x = robot.me.x
-    pos_y = robot.me.y
-    
-    # if carry_fuel > 69 or carry_karb > 17:
-    #     go_home(robot)
-    
-    robot.log('Position is ' + str(pos_x) + ' ' + str(pos_y))
-    ab =  pilgrim_mine(robot)
-    if ab !=0:
-        return ab
-    else:
-        bc = pilgrim_move(robot)
-        if bc !=0:
-            return bc
-    
-
-
-# Crusaders
-def crusaders_move(self):
-    # self.log("Crusader health: " + str(self.me['health']))
-    # The directions: North, NorthEast, East, SouthEast, South, SouthWest, West, NorthWest
-    choices = [(0,-1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)]
-    choice = random.choice(choices)
-    # self.log('TRYING TO MOVE IN DIRECTION ' + str(choice))
-    return choice
-
 
 class MyRobot(BCAbstractRobot):
 
@@ -136,15 +37,16 @@ class MyRobot(BCAbstractRobot):
         unit_preacher = SPECS['PREACHER']
         unit_prophet = SPECS['PROPHET']
 
+        # DEBUG
         # self.log("START TURN " + self.step)
 
         if self.step % 250 == 0:
             self.log("Total current karbonite is " + str(self.karbonite))
-        
-        # if unit_type == unit_crusader:
-        #     return self.move(crusaders_move(self))
 
-        elif unit_type == unit_castle:
+        if unit_type == unit_crusader:
+            return self.move(crusaders_move(self))
+
+        if unit_type == unit_castle:
             if self.step < 5:
                 # self.log("Building a crusader at " + str(self.me['x']+1) + ", " + str(self.me['y']+1))
                 return self.build_unit(unit_pilgrim, 1, 1)
