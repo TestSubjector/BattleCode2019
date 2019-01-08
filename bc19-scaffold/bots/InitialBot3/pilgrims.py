d143c0ffb22f9017ed7cf8db0e4b41751a8e3f48
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2019-01-08 21:48:56 +0530

    IB3 no won't have any stuck pilgrims

--- bc19-scaffold/bots/InitialBot3/pilgrims.py
+++ bc19-scaffold/bots/InitialBot3/pilgrims.py
@@ -1,3 +1,5 @@
+import utility
+
 def pilgrim(robot):
     carry_karb = robot.me.karbonite 
     carry_fuel = robot.me.fuel
@@ -23,57 +25,58 @@ def pilgrim_move(robot):
     karb_map = robot.get_karbonite_map()
     fuel_map = robot.get_fuel_map()
     bounds_map = len(robot.map)
+    occupied_map = robot.get_visible_robot_map()
 
     # May change for impossible resources
     # SE
-    if (pos_x + 1 < bounds_map and pos_y + 1 <bounds_map) and (karb_map[pos_x + 1][pos_y + 1] == 1 or fuel_map[pos_x + 1][pos_y + 1] == 1):
+    if (not utility.is_cell_occupied(occupied_map, pos_x + 1, pos_y + 1))  and (karb_map[pos_y + 1][pos_x + 1] == 1 or fuel_map[pos_y + 1][pos_x + 1] == 1):
         return robot.move(1, 1)
     # E
-    elif (pos_x + 1 < bounds_map) and (karb_map[pos_x + 1][pos_y + 0] == 1 or fuel_map[pos_x + 1][pos_y + 0] == 1):
-        return robot.move(0, 1)
+    elif (not utility.is_cell_occupied(occupied_map, pos_x + 1, pos_y + 0)) and (karb_map[pos_y + 0][pos_x + 1] == 1 or fuel_map[pos_y + 0][pos_x + 1] == 1):
+        return robot.move(1, 0)
     # NE
-    elif (pos_x + 1 < bounds_map and pos_y - 1 >= 0) and (karb_map[pos_x + 1][pos_y - 1] == 1 or fuel_map[pos_x + 1][pos_y - 1] == 1):
-        return robot.move(-1, 1)
+    elif (not utility.is_cell_occupied(occupied_map, pos_x + 1, pos_y - 1)) and (karb_map[pos_y - 1][pos_x + 1] == 1 or fuel_map[pos_y - 1][pos_x + 1] == 1):
+        return robot.move(1, -1)
     # N
-    elif (pos_y - 1 >= 0) and (karb_map[pos_x + 0][pos_y - 1] == 1 or fuel_map[pos_x + 0][pos_y - 1] == 1):
-        return robot.move(-1, 0)
+    elif (not utility.is_cell_occupied(occupied_map, pos_x + 0, pos_y - 1)) and (karb_map[pos_y - 1][pos_x + 0] == 1 or fuel_map[pos_y - 1][pos_x + 0] == 1):
+        return robot.move(0, -1)
     # NW
-    elif (pos_x - 1 >= 0 and pos_y - 1 >= 0) and (karb_map[pos_x - 1][pos_y - 1] == 1 or fuel_map[pos_x - 1][pos_y - 1] == 1):
+    elif (not utility.is_cell_occupied(occupied_map, pos_x - 1, pos_y - 1)) and  (karb_map[pos_y - 1][pos_x - 1] == 1 or fuel_map[pos_y - 1][pos_x - 1] == 1):
         return robot.move(-1, -1)
     # W
-    elif (pos_x - 1 >= 0) and (karb_map[pos_x - 1][pos_y + 0] == 1 or fuel_map[pos_x - 1][pos_y + 0] == 1):
-        return robot.move(0, -1)
+    elif (not utility.is_cell_occupied(occupied_map, pos_x - 1, pos_y + 0)) and  (karb_map[pos_y + 0][pos_x - 1] == 1 or fuel_map[pos_y + 0][pos_x - 1] == 1):
+        return robot.move(-1, 0)
     # SW
-    elif (pos_x - 1 >= 0 and pos_y + 1 < bounds_map) and (karb_map[pos_x - 1][pos_y + 1] == 1 or fuel_map[pos_x - 1][pos_y + 1] == 1):
-        return robot.move(1, -1)
+    elif (not utility.is_cell_occupied(occupied_map, pos_x - 1, pos_y)) and (karb_map[pos_y + 1][pos_x - 1] == 1 or fuel_map[pos_y + 1][pos_x - 1] == 1):
+        return robot.move(-1, 1)
     # S
-    elif (pos_x >= 0 and pos_y + 1 < bounds_map) and (karb_map[pos_x + 0][pos_y + 1] == 1 or fuel_map[pos_x + 0][pos_y + 1] == 1):
-        return robot.move(1, 0)
+    elif (not utility.is_cell_occupied(occupied_map, pos_x + 0, pos_y + 1)) and (karb_map[pos_y + 1][pos_x + 0] == 1 or fuel_map[pos_y + 1][pos_x + 0] == 1):
+        return robot.move(0, 1)
     else:
         # Just move
-        if pos_x + 1 < bounds_map and pos_y + 1 <bounds_map and passable_map[pos_x + 1][pos_y + 1] == 1:
+        if (not utility.is_cell_occupied(occupied_map, pos_x + 1, pos_y + 1)) and  pos_y + 1 <bounds_map and passable_map[pos_y + 1][pos_x + 1] == 1:
             return robot.move(1, 1)
         # E
-        elif pos_x + 1 < bounds_map and passable_map[pos_x + 1][pos_y + 0] == 1:
-            return robot.move(0, 1)
+        elif (not utility.is_cell_occupied(occupied_map, pos_x + 1, pos_y + 0)) and passable_map[pos_y + 0][pos_x + 1] == 1:
+            return robot.move(1, 0)
         # SE
-        elif pos_x + 1 < bounds_map and pos_y - 1 >= 0 and passable_map[pos_x + 1][pos_y - 1] == 1:
-            return robot.move(-1, 1)
+        elif (not utility.is_cell_occupied(occupied_map, pos_x + 1, pos_y - 1)) and passable_map[pos_y - 1][pos_x + 1] == 1:
+            return robot.move(1, -1)
         # S
-        elif pos_y - 1 >= 0 and passable_map[pos_x + 0][pos_y - 1] == 1:
-            return robot.move(-1, 1)
+        elif (not utility.is_cell_occupied(occupied_map, pos_x + 0, pos_y - 1)) and  passable_map[pos_y - 1][pos_x + 0] == 1:
+            return robot.move(0, -1)
         # SW
-        elif pos_x - 1 >= 0 and pos_y - 1 >= 0 and passable_map[pos_x - 1][pos_y - 1] == 1:
+        elif (not utility.is_cell_occupied(occupied_map, pos_x - 1, pos_y - 1)) and passable_map[pos_y - 1][pos_x - 1] == 1:
             return robot.move(-1, -1)
         # W
-        elif pos_x - 1 >= 0 and passable_map[pos_x - 1][pos_y + 0] == 1:
-            return robot.move(0, -1)
+        elif (not utility.is_cell_occupied(occupied_map, pos_x - 1, pos_y + 0)) and passable_map[pos_y + 0][pos_x - 1] == 1:
+            return robot.move(-1, 0)
         # NW
-        elif pos_x - 1 >= 0 and passable_map[pos_x - 1][pos_y + 1] == 1:
+        elif (not utility.is_cell_occupied(occupied_map, pos_x - 1, pos_y + 1)) and passable_map[pos_y + 1][pos_x - 1] == 1:
             return robot.move(-1, 1)
         # N
-        elif pos_x >= 0 and pos_y + 1 < bounds_map and passable_map[pos_x + 0][pos_y + 1] == 1:
-            return robot.move(1, 0)
+        elif (not utility.is_cell_occupied(occupied_map, pos_x + 0, pos_y + 1)) and passable_map[pos_y + 1][pos_x + 0] == 1:
+            return robot.move(0, 1)
         else: 
             return 0
 
@@ -83,7 +86,7 @@ def pilgrim_mine(robot):
 
     karb_map = robot.get_karbonite_map()
     fuel_map = robot.get_fuel_map()
-    if karb_map[pos_x][pos_y] == 1 or fuel_map[pos_x][pos_y] == 1:
+    if karb_map[pos_y][pos_x] == 1 or fuel_map[pos_y][pos_x] == 1:
         return robot.mine()
     else:
         return 0        
