fe8b6c1de96991d07da67233ba0132c883d2f1c3
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2019-01-09 18:29:42 +0530

    Poor changes

--- bc19-scaffold/bots/WorkingBot1/pilgrims.py
+++ bc19-scaffold/bots/WorkingBot1/pilgrims.py
@@ -1,6 +1,6 @@
-from battlecode import SPECS
 import utility
 import vision
+from battlecode import SPECS
 
 def pilgrim(robot):
     carry_karb = robot.me.karbonite
@@ -8,7 +8,7 @@ def pilgrim(robot):
     pos_x = robot.me.x
     pos_y = robot.me.y
 
-    if carry_fuel > 90 or carry_karb > 18 :
+    if carry_fuel > 80 or carry_karb > 16 :
         # robot.log("Nearing capacity")
         return pilgrim_full(robot)
 
@@ -73,13 +73,17 @@ def pilgrim_full(robot):
         friendly_units = vision.sort_visible_friendlies_by_distance(robot)
         if friendly_units:
             for f_unit in friendly_units:
-                if f_unit.unit == unit_church:
-                    if abs(f_unit.x - pos_x) <= 1 and abs(f_unit.y - pos_y) <= 1:
-                        # robot.log("Giving church " + str(f_unit.id) + " " + str(carry_karb) + " karbonite and " + str(carry_fuel) + " fuel.")
-                        # robot.log(str(f_unit))
-                        # robot.log(str(robot.me))
-                        # robot.log(str((f_unit.x - pos_x, f_unit.y - pos_y)))
-                        # return robot.give(f_unit.x - pos_x, f_unit.y - pos_y, carry_karb, carry_fuel)
+                dx = f_unit.x - pos_x
+                dy = f_unit.y - pos_y
+                if f_unit.unit == unit_church and abs(dx) <= 1 and abs(dy) <= 1 and utility.is_cell_occupied(occupied_map, f_unit.x, f_unit.y):
+                    robot.log("Giving church " + str(f_unit.id) + " " + str(carry_karb) + " karbonite and " + str(carry_fuel) + " fuel.")
+                    robot.log(str(f_unit))
+                    robot.log(str(robot.me))
+                    robot.log(str((dx, dy)))
+                    # if utility.is_cell_occupied(occupied_map, pos_x + f_unit.x - pos_x, pos_y + f_unit.y - pos_y):
+                    #     robot.log("Exists")
+                    return robot.give(dx, dy, carry_karb, carry_fuel)
+        
         for direction in directions:
             if (not utility.is_cell_occupied(occupied_map, pos_x + direction[1],  pos_y + direction[0])) and (karb_map[pos_y + direction[0]][pos_x + direction[1]] != 1 or fuel_map[pos_y + direction[0]][pos_x + direction[1]] != 1) and passable_map[pos_y + direction[0]][pos_x + direction[1]] == 1:
                 if robot.karbonite > 50 and robot.fuel > 200:
--- bc19-scaffold/bots/WorkingBot1/utility.py
+++ bc19-scaffold/bots/WorkingBot1/utility.py
@@ -4,7 +4,7 @@ def is_cell_occupied(occupied_map, pos_x, pos_y):
     bounds_map = len(occupied_map)
     if pos_x < 0 or pos_y < 0 or pos_x >= bounds_map or pos_y >= bounds_map:
         return True
-    elif occupied_map[pos_y][pos_x] == 0 or occupied_map[pos_y][pos_x] == -1:
+    elif occupied_map[pos_y][pos_x] <= 0:
         return False
     else:
         return True
