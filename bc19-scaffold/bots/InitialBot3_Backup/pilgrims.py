67c918a806184b25f23ab9cba868586d8f061fe0
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2019-01-09 09:26:02 +0530

    Proper church build

--- bc19-scaffold/bots/InitialBot3_Backup/pilgrims.py
+++ bc19-scaffold/bots/InitialBot3_Backup/pilgrims.py
@@ -8,7 +8,7 @@ def pilgrim(robot):
     pos_y = robot.me.y
 
     if carry_fuel > 90 or carry_karb > 18 :
-        # robot.log("Nearing capacity")
+        robot.log("Nearing capacity")
         return pilgrim_full(robot)
 
     # robot.log('Position is ' + str(pos_x) + ' ' + str(pos_y))
@@ -49,7 +49,7 @@ def pilgrim_move(robot):
     elif (not utility.is_cell_occupied(occupied_map, pos_x - 1, pos_y + 0)) and  (karb_map[pos_y + 0][pos_x - 1] == 1 or fuel_map[pos_y + 0][pos_x - 1] == 1):
         return robot.move(-1, 0)
     # SW
-    elif (not utility.is_cell_occupied(occupied_map, pos_x - 1, pos_y + 1)) and (karb_map[pos_y + 1][pos_x - 1] == 1 or fuel_map[pos_y + 1][pos_x - 1] == 1):
+    elif (not utility.is_cell_occupied(occupied_map, pos_x - 1, pos_y)) and (karb_map[pos_y + 1][pos_x - 1] == 1 or fuel_map[pos_y + 1][pos_x - 1] == 1):
         return robot.move(-1, 1)
     # S
     elif (not utility.is_cell_occupied(occupied_map, pos_x + 0, pos_y + 1)) and (karb_map[pos_y + 1][pos_x + 0] == 1 or fuel_map[pos_y + 1][pos_x + 0] == 1):
@@ -106,30 +106,29 @@ def pilgrim_full(robot):
     occupied_map = robot.get_visible_robot_map()
 
     if karb_map[pos_y][pos_x] == 1 or fuel_map[pos_y][pos_x] == 1:
-        if (not utility.is_cell_occupied(occupied_map, pos_x + 1, pos_y + 1)) and passable_map[pos_y + 1][pos_x + 1] == 1 and (karb_map[pos_y + 1][pos_x + 1] != 1 and fuel_map[pos_y + 1][pos_x + 1] != 1):
+        if (not utility.is_cell_occupied(occupied_map, pos_x + 1, pos_y + 1)) and (karb_map[pos_y + 1][pos_x + 1] != 1 or fuel_map[pos_y + 1][pos_x + 1] != 1):
             store_x, store_y = 1, 1
         # E
-        elif (not utility.is_cell_occupied(occupied_map, pos_x + 1, pos_y + 0)) and passable_map[pos_y + 0][pos_x + 1] == 1 and (karb_map[pos_y + 0][pos_x + 1] != 1 and fuel_map[pos_y + 0][pos_x + 1] != 1):
+        elif (not utility.is_cell_occupied(occupied_map, pos_x + 1, pos_y + 0)) and (karb_map[pos_y + 0][pos_x + 1] != 1 or fuel_map[pos_y + 0][pos_x + 1] != 1):
             store_x, store_y = 1, 0
         # NE
-        elif (not utility.is_cell_occupied(occupied_map, pos_x + 1, pos_y - 1)) and passable_map[pos_y - 1][pos_x + 1] == 1 and (karb_map[pos_y - 1][pos_x + 1] != 1 and fuel_map[pos_y - 1][pos_x + 1] != 1):
+        elif (not utility.is_cell_occupied(occupied_map, pos_x + 1, pos_y - 1)) and (karb_map[pos_y - 1][pos_x + 1] != 1 or fuel_map[pos_y - 1][pos_x + 1] != 1):
             store_x, store_y = 1, -1
         # N
-        elif (not utility.is_cell_occupied(occupied_map, pos_x + 0, pos_y - 1)) and passable_map[pos_y - 1][pos_x + 0] == 1 and (karb_map[pos_y - 1][pos_x + 0] != 1 and fuel_map[pos_y - 1][pos_x + 0] != 1):
+        elif (not utility.is_cell_occupied(occupied_map, pos_x + 0, pos_y - 1)) and (karb_map[pos_y - 1][pos_x + 0] != 1 or fuel_map[pos_y - 1][pos_x + 0] != 1):
             store_x, store_y = 0, -1
         # NW
-        elif (not utility.is_cell_occupied(occupied_map, pos_x - 1, pos_y - 1)) and passable_map[pos_y - 1][pos_x - 1] == 1 and  (karb_map[pos_y - 1][pos_x - 1] != 1 and fuel_map[pos_y - 1][pos_x - 1] != 1):
+        elif (not utility.is_cell_occupied(occupied_map, pos_x - 1, pos_y - 1)) and  (karb_map[pos_y - 1][pos_x - 1] != 1 or fuel_map[pos_y - 1][pos_x - 1] != 1):
             store_x, store_y = -1, -1
         # W
-        elif (not utility.is_cell_occupied(occupied_map, pos_x - 1, pos_y + 0)) and passable_map[pos_y + 0][pos_x - 1] == 1 and  (karb_map[pos_y + 0][pos_x - 1] != 1 and fuel_map[pos_y + 0][pos_x - 1] != 1):
+        elif (not utility.is_cell_occupied(occupied_map, pos_x - 1, pos_y + 0)) and  (karb_map[pos_y + 0][pos_x - 1] != 1 or fuel_map[pos_y + 0][pos_x - 1] != 1):
             store_x, store_y = -1, 0
         # SW
-        elif (not utility.is_cell_occupied(occupied_map, pos_x - 1, pos_y + 1)) and passable_map[pos_y + 1][pos_x - 1] == 1 and (karb_map[pos_y + 1][pos_x - 1] != 1 and fuel_map[pos_y + 1][pos_x - 1] != 1):
+        elif (not utility.is_cell_occupied(occupied_map, pos_x - 1, pos_y)) and (karb_map[pos_y + 1][pos_x - 1] != 1 or fuel_map[pos_y + 1][pos_x - 1] != 1):
             store_x, store_y = -1, 1
         # S
-        elif (not utility.is_cell_occupied(occupied_map, pos_x + 0, pos_y + 1)) and passable_map[pos_y + 1][pos_x + 0] == 1 and (karb_map[pos_y + 1][pos_x + 0] != 1 and fuel_map[pos_y + 1][pos_x + 0] != 1):
+        elif (not utility.is_cell_occupied(occupied_map, pos_x + 0, pos_y + 1)) and (karb_map[pos_y + 1][pos_x + 0] != 1 or fuel_map[pos_y + 1][pos_x + 0] != 1):
             store_x, store_y = 0, 1
 
-        if robot.karbonite > 50 and robot.fuel > 200:
-            robot.log("Drop a church like it's hot")
-            return robot.build_unit(unit_church, store_x, store_y)
+        robot.log("Drop a church like it's hot")
+        return robot.build_unit(unit_church, store_x, store_y)
