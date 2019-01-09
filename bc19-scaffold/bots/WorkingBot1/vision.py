2b9abe9e065313066aec369666116c690d76a23f
diddypod <suchitkar1997@gmail.com>
2019-01-09 14:53:05 +0530

    Give, but sparingly

--- bc19-scaffold/bots/WorkingBot1/vision.py
+++ bc19-scaffold/bots/WorkingBot1/vision.py
@@ -1,6 +1,6 @@
 def sort_visible_units_by_distance(robot):
     visible = robot.get_visible_robots()
-    
+
     bots = []
     bots_distance = []
     for r in visible:
@@ -9,27 +9,29 @@ def sort_visible_units_by_distance(robot):
         # now all in vision range, can see x, y etc
         bots_distance.append((r['x'] - robot.me['x'])**2 + (r['y'] - robot.me['y'])**2)
         bots.append(r)
-    
+
     return [x for _,x in sorted(zip(bots_distance, bots))]
 
 def sort_visible_friendlies_by_distance(robot):
     visible = robot.get_visible_robots()
-    
+
     friendly_bots = []
     friendly_bots_distance = []
+    if visible is None:
+        return []
     for r in visible:
-        if not robot.is_visible(r):
+        if 'x' not in r:
             continue
         # now all in vision range, can see x, y etc
         if r['team'] == robot.me['team']:
-                friendly_bots_distance.append((r['x'] - robot.me['x'])**2 + (r['y'] - robot.me['y'])**2)
-                friendly_bots.append(r)
-    
+            friendly_bots_distance.append((r['x'] - robot.me['x'])**2 + (r['y'] - robot.me['y'])**2)
+            friendly_bots.append(r)
+
     return [x for _,x in sorted(zip(friendly_bots_distance, friendly_bots))]
 
 def sort_visible_enemies_by_distance(robot):
     visible = robot.get_visible_robots()
-    
+
     enemy_bots = []
     enemy_bots_distance = []
     for r in visible:
@@ -37,8 +39,8 @@ def sort_visible_enemies_by_distance(robot):
             continue
         # now all in vision range, can see x, y etc
         if r['team'] != robot.me['team']:
-                enemy_bots_distance.append((r['x'] - robot.me['x'])**2 + (r['y'] - robot.me['y'])**2)
-                enemy_bots.append(r)
-    
+            enemy_bots_distance.append((r['x'] - robot.me['x'])**2 + (r['y'] - robot.me['y'])**2)
+            enemy_bots.append(r)
+
     return [x for _,x in sorted(zip(enemy_bots_distance, enemy_bots))]
 
